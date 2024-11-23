16e88e6b4e734ac9a77f2b316f304477903fa4ba
Fayoka <60096230+Fayoka@users.noreply.github.com>
2024-11-23 01:11:38 +0100

    cleaned up code and added comments

--- ChessByKylian/Game.cs
+++ ChessByKylian/Game.cs
@@ -5,48 +5,58 @@ namespace ChessByKylian
 {
     public static class Game
     {
+    private static bool GameOver = false;
         public static void StartGame()
         {
-            // to-do Bool GameOver
-            
             Console.WriteLine("\nWelcome to ChessByKylian");
             Board.FillStartingBoard();
             Board.RenderBoard();
+            
+            
             // ManageGame
             var turnWhite = true;
-            while (true)
+            
+            while (!GameOver)
             {
-                Console.WriteLine("\nWhat piece would you like to move?");
+                Console.Write("\nWhat piece would you like to move? ");
                 var chosenPiece = Console.ReadLine();
-                var canMove = validMove(chosenPiece, turnWhite);
+                var userInput = ParseInput(chosenPiece);
+                var canMove = validMove(userInput, turnWhite);
                 Console.WriteLine(canMove);
             }
         }
 
-        private static bool validMove(string chosenPiece, bool turnWhite)
+        /// <summary>
+        /// Check if there is a piece and if it can be moved, ex: turn 1 queen can't move because there is a pawn in front of it
+        /// </summary>
+        /// <param name="userMove"></param>
+        /// <param name="turnWhite"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private static bool validMove(UserMove userMove, bool turnWhite)
         {
-            var chosenMoves = new UserMove("null", "null");
-            chosenMoves = ParseInput(chosenPiece);
-            Console.WriteLine("User Input was: " + chosenPiece);
-            Console.WriteLine("\nYou wanna move the position row (" + chosenMoves.Row + ") and column (" + chosenMoves.Column + ").");
-            return false;
+            throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// Parse the input string given by the user
+        /// </summary>
+        /// <param name="moveChosen"></param>
+        /// <returns></returns>
         private static UserMove ParseInput(string moveChosen)
         {
-            if (CheckValidInput(moveChosen))
-            {
-                var success = int.TryParse(moveChosen[1].ToString(), out var score);
-                return success ? new UserMove(moveChosen[1].ToString(), moveChosen[0].ToString()) : new UserMove(moveChosen[0].ToString(), moveChosen[1].ToString());
-            }
-
-            return new UserMove("null", "null");
+            if (!CheckValidInput(moveChosen)) return new UserMove("null", "null");
+            var success = int.TryParse(moveChosen[1].ToString(), out var score);
+            return success ? new UserMove(moveChosen[1].ToString(), moveChosen[0].ToString()) : new UserMove(moveChosen[0].ToString(), moveChosen[1].ToString());
         }
-
+        /// <summary>
+        /// Check if it was given valid input string
+        /// </summary>
+        /// <param name="moveChosen"></param>
+        /// <returns></returns>
         private static bool CheckValidInput(string moveChosen)
         {
-            string pattern = @"^[a-hA-H]\d$|^\d[a-hA-H]$";
-            bool isMatch = Regex.IsMatch(moveChosen, pattern);
+            var pattern = @"^[a-hA-H]\d$|^\d[a-hA-H]$";
+            var isMatch = Regex.IsMatch(moveChosen, pattern);
 
             if (isMatch)
             {
